1f2933274e5cf6f906c48b75d61d71ab2532a558
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2021-01-08 14:10:28 +0100

    Some update

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,19 @@
 <script>
   let data = [
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1],
-    [0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
-    [0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+[0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+[0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0],
+[0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0],
+[0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+[0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
   ];
 
   // $: setTimeout(() => {
@@ -15,13 +24,13 @@
 <style>
   header {
     /* height: 300px; */
-    background-color: #036;
-    padding: 2px 0;
+    background-color: #123;
+    padding: 12px 0;
   }
 
   .container {
     /* max-width: 1100px; */
-    width: 580px;
+    width: 1000px;
     margin: 0 auto;
   }
 
@@ -31,19 +40,42 @@
     display: block;
     float: none;
     box-sizing: border-box;
+    /* animation: 4s cubic-bezier(0.45, 0, 0.55, 1) 12 alternate rot; */
   }
 
   .row > div {
     display: block;
     float: left;
-    width: 40px;
-    height: 40px;
+    width: 20px;
+    height: 20px;
     margin: 2px;
     background-color: #369;
     box-sizing: border-box;
   }
   .row > div.hi {
     animation: 1s cubic-bezier(0.45, 0, 0.55, 1) 12 alternate bub;
+  }
+
+  .row > div.hi2 {
+    animation: 3s cubic-bezier(0.45, 0, 0.55, 1) 12 forwards rot;
+  }
+
+  @keyframes rot {
+    0% {
+      transform: rotate(0) scale(1);
+      background-color: #369;
+      border-radius: 0;
+    }
+    50% {
+      transform: rotate(90deg) scale(.35);
+      background-color: #fff;
+      border-radius: 20px;
+    }
+    100% {
+      transform: rotate(180deg) scale(1);
+      background-color: #369;
+      border-radius: 0;
+    }
   }
 
   @keyframes bub {
@@ -58,19 +90,21 @@
       background-color: #369;
     }
     100% {
-      border-radius: 40px;
+      border-radius: 30px;
       transform: scale(0.5);
-      background-color: rgb(188, 224, 255);
+      background-color: #fff;
     }
   }
 </style>
 
 <header>
   <div class="container">
-    {#each data as r}
+    {#each data as r, i}
       <div class="row">
-        {#each r as c, i}
-          <div class:hi={!!c} style="animation-delay: {i * 25}ms" />
+        {#each r as c, j}
+          <div
+            class:hi2={c === 1}
+            style="animation-delay: {j * 15 + i * 5}ms" />
         {/each}
       </div>
     {/each}
