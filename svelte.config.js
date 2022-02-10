a9a4531f53b86df50bc32c7f6faecf1b001ab165
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2022-02-10 21:06:10 +0100

    Fix config

--- svelte.config.js
+++ svelte.config.js
@@ -14,9 +14,16 @@ const config = {
 			assets: 'build',
 			fallback: null
 		})
-
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte'
+	},
+	files: {
+		assets: 'static',
+		lib: 'src/lib'
+	},
+	paths: {
+		assets: '',
+		base: ''
 	}
 };
 
