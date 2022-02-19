342deb74e8bd4e68822b2c4da18b1b1b556e2c9f
Nicolas Le Thierry d'Ennequin <nlte@magic.fr>
2022-02-19 21:52:44 +0100

    Some

--- src/routes/system-reset.svelte
+++ src/routes/system-reset.svelte
@@ -1,15 +1,36 @@
-<div class="container">
+<div class="container img">
 	<img src="../img/2-1.jpg" alt="System reset" />
+</div>
+
+<div class="container">
+	<article>
+		<p>
+			<strong>Are you certain?</strong> Well, think twice before pressing that key. Blue keys have that
+			unassuming &mdash; unominous &mdash; thing about them, but... bad consequences can never be ruled
+			out.
+		</p>
+	</article>
 </div>
 
 <style>
 	img {
 		width: 100%;
+		margin-bottom: 12px;
+	}
+
+	article {
+		background-color: #fff;
+		padding: 24px;
+		min-height: 200vh;
 	}
 
 	.container {
-		max-width: 100%;
 		width: 800px;
+		max-width: calc(100vw - 12px);
 		margin: 0 auto;
+	}
+
+	.img.container {
+		max-width: calc(100vw - 24px);
 	}
 </style>
