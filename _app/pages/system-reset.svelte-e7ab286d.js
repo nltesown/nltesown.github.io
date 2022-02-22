5e51ff9297d1c1b831fdf58052f8e3a2831b7eb5
nltesown <nltesown@users.noreply.github.com>
2022-02-22 11:00:50 +0000

    deploy: 434127f8cb4b1af8afe2076fc58454dbc752d595

---
+++ _app/pages/system-reset.svelte-e7ab286d.js
@@ -0,0 +1,29 @@
+import{S as M,i as T,s as V,e as m,c as p,a as f,d,Z as U,b as i,f as b,g as G,G as l,$ as O,K as D,w as R,k,t as P,x as A,m as E,h as z,y as B,q as K,o as Z,B as F}from"../chunks/vendor-a8cf052c.js";function H(u){let e,t,r,o,n;return{c(){e=m("div"),t=m("img"),this.h()},l(a){e=p(a,"DIV",{class:!0,style:!0});var s=f(e);t=p(s,"IMG",{src:!0,alt:!0,style:!0,class:!0}),s.forEach(d),this.h()},h(){U(t.src,r=u[0])||i(t,"src",r),i(t,"alt",u[1]),b(t,"transition",u[3]+"ms"),i(t,"class","svelte-1yxhjwd"),i(e,"class","image-container svelte-1yxhjwd"),b(e,"padding-bottom",1/u[2]*100+"%")},m(a,s){G(a,e,s),l(e,t),o||(n=O(J.call(null,t)),o=!0)},p(a,[s]){s&1&&!U(t.src,r=a[0])&&i(t,"src",r),s&2&&i(t,"alt",a[1]),s&8&&b(t,"transition",a[3]+"ms"),s&4&&b(e,"padding-bottom",1/a[2]*100+"%")},i:D,o:D,d(a){a&&d(e),o=!1,n()}}}function J(u){u.style.opacity=1}function N(u,e,t){let{src:r}=e,{alt:o}=e,{ratio:n=4/3}=e,{duration:a=0}=e;return u.$$set=s=>{"src"in s&&t(0,r=s.src),"alt"in s&&t(1,o=s.alt),"ratio"in s&&t(2,n=s.ratio),"duration"in s&&t(3,a=s.duration)},[r,o,n,a]}class Q extends M{constructor(e){super();T(this,e,N,H,V,{src:0,alt:1,ratio:2,duration:3})}}function W(u){let e,t,r,o,n,a,s,q,h,x,j,v,I,y;return r=new Q({props:{src:"../img/2-1.jpg",alt:"System reset"}}),{c(){e=m("div"),t=m("div"),R(r.$$.fragment),o=k(),n=m("article"),a=m("p"),s=P(`Perc\xE9e de deux fen\xEAtres perpendiculaires, une pi\xE8ce d'angle aux murs mats et tr\xE8s
+			blancs\xA0: une lumi\xE8re d'une nettet\xE9 id\xE9ale, \xE0 l'approche de midi tout particuli\xE8rement,
+			une lumi\xE8re photographique. C'est dans cette pi\xE8ce, au milieu la table ronde, qu'il se trouve.
+			L\xE0, on peut l'examiner \xE0 loisir, on se dit qu'il y a \xE9t\xE9 mis pour \xE7a. Mais une autre fois, il
+			n'y sera plus. Il sera en haut du petit buffet en bois exotique, sa place plus habituelle, \xE0
+			c\xF4t\xE9 d'autres choses, un vase, un cendrier, cinq d\xE9s de poker dice align\xE9s dans leur petite
+			bo\xEEte transparente un peu ray\xE9e par l'usage, de la petite monnaie, trois cl\xE9s piqu\xE9es de
+			rouille attach\xE9es ensemble \xE0 un anneau.`),q=k(),h=m("p"),x=P(`On pourrait \xE9num\xE9rer encore ces choses qui se sont un jour trouv\xE9es l\xE0, sur le haut du buffet,
+			mais qui n'y sont pas. Un lien semble exister entre ces choses pr\xE9sentes et absentes. Tous les
+			jours, certaines sont d\xE9plac\xE9es, prises, rang\xE9es, pos\xE9es, empoch\xE9es, remises, vol\xE9es m\xEAme.
+			(Dans cette maison, on y vole.)`),j=k(),v=m("p"),I=P(`Pour l'heure, il est au centre de la table, au centre de la pi\xE8ce, dans le jour intense dont
+			le souvenir veut l'entourer pour mieux le voir et le d\xE9crire sans erreur. Le soleil est hors
+			de vue, s\xFBrement au-dessus de la maison. Plus t\xF4t dans la matin\xE9e, le soleil entrait par l'une
+			des fen\xEAtres. Un rayon frappait l'objet en plein. Il aurait peut-\xEAtre fallu le voir \xE0 ce
+			moment-l\xE0.`),this.h()},l(c){e=p(c,"DIV",{class:!0});var _=f(e);t=p(_,"DIV",{class:!0});var L=f(t);A(r.$$.fragment,L),L.forEach(d),o=E(_),n=p(_,"ARTICLE",{class:!0});var g=f(n);a=p(g,"P",{class:!0});var w=f(a);s=z(w,`Perc\xE9e de deux fen\xEAtres perpendiculaires, une pi\xE8ce d'angle aux murs mats et tr\xE8s
+			blancs\xA0: une lumi\xE8re d'une nettet\xE9 id\xE9ale, \xE0 l'approche de midi tout particuli\xE8rement,
+			une lumi\xE8re photographique. C'est dans cette pi\xE8ce, au milieu la table ronde, qu'il se trouve.
+			L\xE0, on peut l'examiner \xE0 loisir, on se dit qu'il y a \xE9t\xE9 mis pour \xE7a. Mais une autre fois, il
+			n'y sera plus. Il sera en haut du petit buffet en bois exotique, sa place plus habituelle, \xE0
+			c\xF4t\xE9 d'autres choses, un vase, un cendrier, cinq d\xE9s de poker dice align\xE9s dans leur petite
+			bo\xEEte transparente un peu ray\xE9e par l'usage, de la petite monnaie, trois cl\xE9s piqu\xE9es de
+			rouille attach\xE9es ensemble \xE0 un anneau.`),w.forEach(d),q=E(g),h=p(g,"P",{class:!0});var C=f(h);x=z(C,`On pourrait \xE9num\xE9rer encore ces choses qui se sont un jour trouv\xE9es l\xE0, sur le haut du buffet,
+			mais qui n'y sont pas. Un lien semble exister entre ces choses pr\xE9sentes et absentes. Tous les
+			jours, certaines sont d\xE9plac\xE9es, prises, rang\xE9es, pos\xE9es, empoch\xE9es, remises, vol\xE9es m\xEAme.
+			(Dans cette maison, on y vole.)`),C.forEach(d),j=E(g),v=p(g,"P",{class:!0});var S=f(v);I=z(S,`Pour l'heure, il est au centre de la table, au centre de la pi\xE8ce, dans le jour intense dont
+			le souvenir veut l'entourer pour mieux le voir et le d\xE9crire sans erreur. Le soleil est hors
+			de vue, s\xFBrement au-dessus de la maison. Plus t\xF4t dans la matin\xE9e, le soleil entrait par l'une
+			des fen\xEAtres. Un rayon frappait l'objet en plein. Il aurait peut-\xEAtre fallu le voir \xE0 ce
+			moment-l\xE0.`),S.forEach(d),g.forEach(d),_.forEach(d),this.h()},h(){i(t,"class","image-container svelte-uaz0ko"),i(a,"class","svelte-uaz0ko"),i(h,"class","svelte-uaz0ko"),i(v,"class","svelte-uaz0ko"),i(n,"class","svelte-uaz0ko"),i(e,"class","container svelte-uaz0ko")},m(c,_){G(c,e,_),l(e,t),B(r,t,null),l(e,o),l(e,n),l(n,a),l(a,s),l(n,q),l(n,h),l(h,x),l(n,j),l(n,v),l(v,I),y=!0},p:D,i(c){y||(K(r.$$.fragment,c),y=!0)},o(c){Z(r.$$.fragment,c),y=!1},d(c){c&&d(e),F(r)}}}class Y extends M{constructor(e){super();T(this,e,null,W,V,{})}}export{Y as default};
