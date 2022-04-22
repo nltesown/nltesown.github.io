4eb908c21a48f747d08609304d000a237c9e9269
NLTESOWN <nlte@fastmail.com>
2022-04-22 08:19:30 +0200

    Update calendar data

--- src/routes/cinematheque/calendrier.svelte
+++ src/routes/cinematheque/calendrier.svelte
@@ -19,9 +19,8 @@
 			new Promise((resolve) => {
 				setTimeout(resolve, 500); // Délai minimal de résolution des promesses.
 			}),
-			get('PROG99 Décembre 2021-février 2022/PROG99_GLOBAL/PROG99_SEANCES_DEF.json'),
-			get('PROG111 Mars-mai 2022/PROG111_GLOBAL/PROG111_SEANCES_DEF.json'),
-			get('PROG116 FIFR 2022/PROG116_GLOBAL/PROG116_SEANCES.json')
+			get('PROG116 FIFR 2022/PROG116_GLOBAL/PROG116_SEANCES.json'),
+			get('PROG119 Juin-juillet 2022/PROG119_GLOBAL/PROG119_SEANCES.json')
 		])
 			.then((data) => {
 				data = _(data).filter().value(); // Supprime l'item `undefined` renvoyé par la promesse de délai.
