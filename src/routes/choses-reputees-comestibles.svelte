1c104dff327c6daf4b8868ebf9d4b1bf9796b539
NLTESOWN <nlte@fastmail.com>
2022-05-24 18:53:13 +0200

    Some

--- src/routes/choses-reputees-comestibles.svelte
+++ src/routes/choses-reputees-comestibles.svelte
@@ -1,5 +1,15 @@
+<script>
+	function colorize(el) {
+		let c = 0;
+		el.querySelectorAll('p').forEach((p) => {
+			if (c <= 9) c = c + 1;
+			p.style.color = `#${c}${c}${c}`;
+		});
+	}
+</script>
+
 <div class="container">
-	<article>
+	<article use:colorize>
 		<p>
 			Ça commence avec la bombe roulant lourdement sur le sol du restaurant. Noire comme un boulet
 			de canon, elle fait son entrée comme sur une piste de bowling, dans un roulement sourd, tandis
@@ -9,19 +19,19 @@
 			puis, inexplicablement, la bombe n'explose pas&nbsp;–&nbsp;pas encore, du moins&nbsp;–&nbsp;et
 			c'est le sauve-qui-peut général, bousculade, dispersion. Peu après&nbsp;: la police arrive,
 			les pompiers, l'équipe de déminage, la brigade scientifique. On constate que la mèche s'est
-			éteinte. Tout ce que je sais, je l'ai appris par le journal, qui parle d'un règlement de
-			compte mafieux, ou au moins d'un acte d'intimidation mafieux.
+			éteinte. Tout ce que je sais, je l'ai appris par le journal, qui parlait d'un règlement de
+			compte mafieux, ou d'un acte d'intimidation.
 		</p>
 		<p>
 			Le fait divers m'est rappelé quelque mois plus tard par mes commensaux, alors que nous dînons
-			dans un restaurant italien. Mais oui, je m'en souviens&nbsp;! C'était ici&nbsp;? Ici
+			dans un restaurant italien. Mais oui, je m'en souviens&nbsp;! C'était ici, pas vrai&nbsp;? Ici
 			même&nbsp;!
 		</p>
 		<p>
 			Je regarde autour de moi, et le rappel de cette affaire de la bombe qui roule à travers le
-			restaurant me permet enfin de mettre des mots sur une impression qui me titille depuis que je
-			suis entré dans l'établissement&nbsp;: je n'ai jamais déjeuné ni dîné dans ce restaurant
-			italien ouvert récemment, mais je connais l'endroit, j'en suis certain, la disposition des
+			restaurant me permet enfin de mettre des mots sur une impression qui semble m'aiguillonner
+			depuis que je suis entré dans l'établissement&nbsp;: je n'ai jamais déjeuné ni dîné dans ce
+			restaurant italien, mais je connais l'endroit, j'en suis certain, la disposition des
 			lieux&nbsp;: la grande salle carrée, la mezzanine, le grand zinc à droite en entrant, les
 			grandes baies vitrées sur deux rues perpendiculaires, l'entrée d'angle surélévée de quelques
 			marches depuis la rue. Et je visualise aussi la bombe faisant irruption, lancée depuis
@@ -34,14 +44,14 @@
 			Je ne savais pas la suite de l'histoire. Peu après la bombe, me raconte Cruz, le restaurant a
 			dû fermer, et très peu de temps après un autre a rouvert à sa place. Un autre ou le
 			même&nbsp;? J'interroge Cruz et Blanka en plissant les yeux contre la fumée conjointe de leur
-			cigarette. J'avais oublié, leur dis-je, mais j'ai déjà dîné ici. Ça m'est revenu quand vous
-			avez parlé de la bombe. Je suis venu une fois dans le restaurant d'avant, quelques semaines
-			avant la bombe, et maintenant celui où nous nous trouvons ne me semble pas très différent.
+			cigarette. J'avais oublié, leur dis-je, j'ai déjà dîné ici. Ça m'est revenu quand vous avez
+			parlé de la bombe. Je suis venu une fois dans le restaurant d'avant, quelques semaines avant
+			la bombe, et maintenant celui où nous nous trouvons ne me semble pas très différent.
 		</p>
 		<p>
 			C'est vrai, concède Blanka, à ceci près que ce n'était pas un restaurant italien mais un
-			restaurant indien. Comment es-ce qu'il s'appelait, déjà&nbsp;? Tu y étais allé&nbsp;?
-			demande-t-elle à son compagnon.
+			restaurant indien. Comment s'appelait-il, déjà&nbsp;? Tu y étais allé&nbsp;? demande-t-elle à
+			son compagnon.
 		</p>
 		<p>
 			La bombe avait roulé dans un restaurant indien. Cela aussi, je l'avais oublié. Pas très
@@ -54,7 +64,7 @@
 
 <style>
 	article {
-		padding: 24px;
+		padding: 24px 12px;
 		min-height: 200vh;
 		font-family: 'Spectral', sans-serif;
 		background-color: #fff;
@@ -66,7 +76,7 @@
 		max-width: 40rem;
 		line-height: 1.75;
 		letter-spacing: 0;
-		font-size: 1rem;
+		font-size: 1.25rem;
 	}
 
 	.container {
