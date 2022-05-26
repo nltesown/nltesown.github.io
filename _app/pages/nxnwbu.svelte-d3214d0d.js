752d65cbdef0b9f99487ae075fd1bd5d289a5b80
nltesown <nltesown@users.noreply.github.com>
2022-05-26 11:23:49 +0000

    deploy: 11e578f3809bf7c936bdb3282d9c2e3d9780be86

---
+++ _app/pages/nxnwbu.svelte-d3214d0d.js
@@ -0,0 +1,35 @@
+import{S as X,i as Y,s as Z,e as o,t as n,k as R,c as u,a as c,h as s,d as l,m as z,b as q,g as ee,I as e,a5 as te,j as ne,J as W}from"../chunks/vendor-ff1b9e40.js";function se(_){let i,t,r,v,y,g,d,T,E,w,D,k,a,G,x,j,P,C,I,$,N,J,L,S,f,A,Q,B;return{c(){i=o("div"),t=o("article"),r=o("div"),v=n(_[0]),y=n(" caract\xE8res."),g=R(),d=o("p"),T=n("C'est sur un nom prof\xE9r\xE9 que s'ouvre \u2013\xA0ou peut s'en faut\xA0\u2013 "),E=o("em"),w=n("North by Northwest"),D=n(`. Nous sommes dans le hall d'un immeuble de Manhattan \xE0 l'heure de la sortie des bureaux.
+			Dans le quartier, on vient de voir, en une succession de plans d'ensemble, la foule
+			ininterrompue de la fin d'apr\xE8s-midi se faire avaler par une bouche de m\xE9tro, se laisser
+			porter comme un banc de poissons dans l'escalier monumental de Grand Central. Et maintenant,
+			dans l'immeuble, un ascenseur d\xE9livre \xE0 son tour son chargement d'employ\xE9s. Parmi ceux-ci,
+			pris dans le mouvement g\xE9n\xE9ral, un homme en costume gris moyen continue, imperturbable, de
+			dicter \xE0 sa secr\xE9taire quand un portier le voit passer \xE0 sa hauteur et l'interpelle, lui parmi
+			tous\xA0:\xA0\xAB\xA0Mr. Thornhill\xA0!\xA0\xBB`),k=R(),a=o("p"),G=n("L'"),x=o("em"),j=n("executive"),P=n(` r\xE9plique \xE0 la salutation par un vague bon mot mais ne s'arr\xEAte pas, ne
+			ralentit m\xEAme pas, port\xE9 par le flux des employ\xE9s et son `),C=o("em"),I=n("flow"),$=n(` \xE0 lui, ce bourdonnement
+			de consignes dont il abreuve sa secr\xE9taire. Tandis qu'ils sont port\xE9s vers la sortie, d'autres
+			ascenseurs encore se d\xE9versent \xE0 l'identique. Dans la tr\xE9pidation qui impulse les premi\xE8res
+			minutes de
+			`),N=o("em"),J=n("North by Northwest"),L=n(`, il faudrait peut-\xEAtre suspendre ce bref instant o\xF9 un nom,
+			\xAB\xA0Thornhill\xA0\xBB, est prononc\xE9. Celui qui nous est ainsi d\xE9sign\xE9, Cary Grant dans son
+			costume gris, n'h\xE9siterait sans doute pas \xE0 dire, comme Don Quichotte\xA0: \xAB\xA0Je sais
+			qui je suis\xA0\xBB, mais quel inconv\xE9nient y aurait-il \xE0 ce qu'un autre vous le
+			rappelle\xA0?`),S=R(),f=o("p"),A=n(`Cet instant anodin, c'est lui qui jette les d\xE9s du r\xE9cit, peut-\xEAtre m\xEAme son v\xE9ritable
+			MacGuffin, l'annonce encore ind\xE9celable des avanies qui attendent Thornhill et son nom.`),this.h()},l(h){i=u(h,"DIV",{class:!0});var b=c(i);t=u(b,"ARTICLE",{class:!0});var m=c(t);r=u(m,"DIV",{class:!0});var V=c(r);v=s(V,_[0]),y=s(V," caract\xE8res."),V.forEach(l),g=z(m),d=u(m,"P",{class:!0});var M=c(d);T=s(M,"C'est sur un nom prof\xE9r\xE9 que s'ouvre \u2013\xA0ou peut s'en faut\xA0\u2013 "),E=u(M,"EM",{});var F=c(E);w=s(F,"North by Northwest"),F.forEach(l),D=s(M,`. Nous sommes dans le hall d'un immeuble de Manhattan \xE0 l'heure de la sortie des bureaux.
+			Dans le quartier, on vient de voir, en une succession de plans d'ensemble, la foule
+			ininterrompue de la fin d'apr\xE8s-midi se faire avaler par une bouche de m\xE9tro, se laisser
+			porter comme un banc de poissons dans l'escalier monumental de Grand Central. Et maintenant,
+			dans l'immeuble, un ascenseur d\xE9livre \xE0 son tour son chargement d'employ\xE9s. Parmi ceux-ci,
+			pris dans le mouvement g\xE9n\xE9ral, un homme en costume gris moyen continue, imperturbable, de
+			dicter \xE0 sa secr\xE9taire quand un portier le voit passer \xE0 sa hauteur et l'interpelle, lui parmi
+			tous\xA0:\xA0\xAB\xA0Mr. Thornhill\xA0!\xA0\xBB`),M.forEach(l),k=z(m),a=u(m,"P",{class:!0});var p=c(a);G=s(p,"L'"),x=u(p,"EM",{});var H=c(x);j=s(H,"executive"),H.forEach(l),P=s(p,` r\xE9plique \xE0 la salutation par un vague bon mot mais ne s'arr\xEAte pas, ne
+			ralentit m\xEAme pas, port\xE9 par le flux des employ\xE9s et son `),C=u(p,"EM",{});var K=c(C);I=s(K,"flow"),K.forEach(l),$=s(p,` \xE0 lui, ce bourdonnement
+			de consignes dont il abreuve sa secr\xE9taire. Tandis qu'ils sont port\xE9s vers la sortie, d'autres
+			ascenseurs encore se d\xE9versent \xE0 l'identique. Dans la tr\xE9pidation qui impulse les premi\xE8res
+			minutes de
+			`),N=u(p,"EM",{});var O=c(N);J=s(O,"North by Northwest"),O.forEach(l),L=s(p,`, il faudrait peut-\xEAtre suspendre ce bref instant o\xF9 un nom,
+			\xAB\xA0Thornhill\xA0\xBB, est prononc\xE9. Celui qui nous est ainsi d\xE9sign\xE9, Cary Grant dans son
+			costume gris, n'h\xE9siterait sans doute pas \xE0 dire, comme Don Quichotte\xA0: \xAB\xA0Je sais
+			qui je suis\xA0\xBB, mais quel inconv\xE9nient y aurait-il \xE0 ce qu'un autre vous le
+			rappelle\xA0?`),p.forEach(l),S=z(m),f=u(m,"P",{class:!0});var U=c(f);A=s(U,`Cet instant anodin, c'est lui qui jette les d\xE9s du r\xE9cit, peut-\xEAtre m\xEAme son v\xE9ritable
+			MacGuffin, l'annonce encore ind\xE9celable des avanies qui attendent Thornhill et son nom.`),U.forEach(l),m.forEach(l),b.forEach(l),this.h()},h(){q(r,"class","count svelte-k34rbs"),q(d,"class","svelte-k34rbs"),q(a,"class","svelte-k34rbs"),q(f,"class","svelte-k34rbs"),q(t,"class","svelte-k34rbs"),q(i,"class","container svelte-k34rbs")},m(h,b){ee(h,i,b),e(i,t),e(t,r),e(r,v),e(r,y),e(t,g),e(t,d),e(d,T),e(d,E),e(E,w),e(d,D),e(t,k),e(t,a),e(a,G),e(a,x),e(x,j),e(a,P),e(a,C),e(C,I),e(a,$),e(a,N),e(N,J),e(a,L),e(t,S),e(t,f),e(f,A),Q||(B=te(_[1].call(null,t)),Q=!0)},p(h,[b]){b&1&&ne(v,h[0])},i:W,o:W,d(h){h&&l(i),Q=!1,B()}}}function ae(_,i,t){let r=0;function v(y){y.querySelectorAll("p").forEach(g=>{t(0,r=r+g.textContent.replace(/\\[nrt]/gi,"").length)})}return[r,v]}class le extends X{constructor(i){super();Y(this,i,ae,se,Z,{})}}export{le as default};
