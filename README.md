ec3fe2fe2542f9459b170eae55e28da07ec57ad8
NLTESOWN <nlte@fastmail.com>
2022-05-26 16:51:45 +0200

    Edit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NLTESOWN
+
+git add -A && git commit -m "Edit" && git checkout master && git checkout master && git push origin dev master && git checkout dev
--- src/routes/nxnwbu.svelte
+++ src/routes/nxnwbu.svelte
@@ -16,37 +16,39 @@
 				>North by Northwest</em
 			>. Nous sommes dans le hall d'un immeuble de Manhattan à l'heure de la sortie des bureaux.
 			Dans le quartier, on vient de voir, en une succession de plans d'ensemble, la foule
-			ininterrompue de la fin d'après-midi se faire avaler par une bouche de métro, se laisser
-			porter comme un banc de poissons dans l'escalier monumental de Grand Central. Et maintenant,
-			dans l'immeuble, un ascenseur délivre à son tour son chargement d'employés. Parmi ceux-ci,
-			pris dans le mouvement général, un homme continue, imperturbable, de dicter à sa secrétaire
-			quand un portier le voit passer à sa hauteur et l'interpelle, lui parmi
+			ininterrompue de la fin d'après-midi traverser une avenue, descendre dans une bouche de métro,
+			se laisser porter comme un banc de poissons dans l'escalier monumental de Grand Central. Ne
+			dirait-on l'incarnation idéale d'une foule affairée, démocratique, immanquablement américaine,
+			où conformité et «&nbsp;distinction&nbsp;» vont de pair, où l'anonymat consenti offre en même
+			temps la possibilité de l'affirmation individuelle&nbsp;? Et maintenant, dans l'immeuble, un
+			ascenseur délivre à son tour son chargement d'employés. Parmi ceux-ci, pris dans le mouvement
+			général, un homme continue, imperturbable, de dicter à sa secrétaire quand un portier le voit
+			passer à sa hauteur et l'interpelle avec enjouement, lui seul parmi
 			tous&nbsp;:&nbsp;«&nbsp;Mr. Thornhill&nbsp;!&nbsp;»
 		</p>
 		<p>
-			L'homme élégant en costume gris réplique à la salutation par un vague bon mot mais ne s'arrête
-			pas, ne ralentit même pas, porté par le flux des employés et son propre <em>flow</em>, ce
+			L'homme en costume gris réplique à la salutation par un vague bon mot mais ne s'arrête pas, ne
+			ralentit même pas, porté par le flot des employés et son propre <em>flow</em>, ce
 			bourdonnement de consignes dont il abreuve sa secrétaire. Tandis qu'ils sont portés vers la
 			sortie, d'autres ascenseurs encore se déversent à l'identique.
 		</p>
 		<p>
-			Dans la trépidation qui donne son impulsion aux premières minutes de
-			<em>North by Northwest</em>, il faudrait peut-être suspendre ce bref instant où un nom,
-			«&nbsp;Thornhill&nbsp;», est prononcé. Celui qui nous est ainsi désigné, Cary Grant évidemment
-			impeccable dans son costume gris, n'hésiterait sans doute pas à dire, comme Don
-			Quichotte&nbsp;: «&nbsp;Je sais qui je suis&nbsp;», mais quel inconvénient y aurait-il à ce
-			qu'un autre vous le rappelle&nbsp;? On ne peut pas le savoir, mais cet instant est celui qui,
-			en faisant nommer son protagoniste, amorce véritablement le récit (ne serait-ce pas même son
-			véritable MacGuffin&nbsp;?), un récit qui va s'entêter à vouloir le dépouiller de son nom et
-			de son identité.
+			Dans le cours du mouvement qui donne son impulsion aux premières minutes de
+			<em>North by Northwest</em>, il faudrait suspendre ce bref instant où ce nom,
+			«&nbsp;Thornhill&nbsp;», est prononcé. Celui qui nous est ainsi désigné, Cary Grant impeccable
+			dans son costume gris, n'hésiterait sans doute pas à dire, comme Don Quichotte&nbsp;:
+			«&nbsp;Je sais qui je suis&nbsp;», mais quel inconvénient y aurait-il à ce qu'un autre vous le
+			rappelle&nbsp;? On l'ignore encore, mais cet instant, en faisant nommer son protagoniste,
+			amorce véritablement le récit (ne serait-ce pas même son véritable MacGuffin&nbsp;?), un récit
+			qui va s'entêter à vouloir le dépouiller de son nom et de son identité.
 		</p>
 		<p>
 			Peu après, Thornhill, ayant rejoint des relations d'affaires au bar de l'hôtel Plaza, fait
 			innocemment le geste qui referme sur lui un piège auquel il est étranger&nbsp;: il lève la
-			main pour appeler un serveur à l'instant même où celui-ci passe au milieu des clients en
-			appelant un nom. Pour les deux inconnus qui ont monté le stratagème et qui observent la scène,
-			Thornhill devient celui qu'ils cherchent, le nommé Kaplan. Et l'ayant ainsi identifié, le
-			kidnappent.
+			main pour appeler un garçon à l'instant même où celui-ci passe au milieu des clients en
+			appelant un nom. Pour les deux inconnus qui ont monté le stratagème et qui observent la scène
+			à l'écart, Thornhill devient celui qu'ils cherchent, un nommé Kaplan. Et l'ayant ainsi
+			identifié, le kidnappent.
 		</p>
 	</article>
 </div>
