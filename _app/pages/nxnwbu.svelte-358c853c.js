cf4ba91eda8eee72f0a46efa2a8ddb06efeee267
nltesown <nltesown@users.noreply.github.com>
2022-05-27 13:41:09 +0000

    deploy: e40ca647680b3770af0fe651a20431d2bbcd0ed8

---
+++ _app/pages/nxnwbu.svelte-358c853c.js
@@ -0,0 +1,61 @@
+import{S as te,i as ne,s as ae,e as i,t as n,k as C,c as l,a as o,h as a,d as s,m as j,b as f,g as se,I as e,a5 as re,j as ie,J as ee}from"../chunks/vendor-ff1b9e40.js";function le(y){let u,t,r,b,E,v,d,w,x,D,A,M,G,L,$,c,H,N,I,J,S,p,z,T,K,O,Q,q,V,R,F;return{c(){u=i("div"),t=i("article"),r=i("div"),b=n(y[0]),E=n(" caract\xE8res."),v=C(),d=i("p"),w=n("C'est sur un nom prononc\xE9 que s'ouvre \u2013\xA0ou peut s'en faut\xA0\u2013 "),x=i("em"),D=n("North by Northwest"),A=n(`
+			(`),M=i("em"),G=n("La Mort aux trousses"),L=n(`, Alfred Hitchcock, 1959). Nous sommes dans le hall d'un
+			immeuble de Manhattan \xE0 l'heure de la sortie des bureaux. Dans le quartier, on vient de voir,
+			en une succession de plans d'ensemble, la foule ininterrompue de la fin d'apr\xE8s-midi traverser
+			une avenue, descendre dans une bouche de m\xE9tro, se laisser porter comme un banc de poissons
+			dans l'escalier monumental de Grand Central. [Ne dirait-on l'incarnation id\xE9ale d'une foule
+			affair\xE9e, d\xE9mocratique, immanquablement am\xE9ricaine, o\xF9 conformit\xE9 et \xAB\xA0distinction\xA0\xBB
+			vont de pair, o\xF9 l'anonymat consenti offre en m\xEAme temps la possibilit\xE9 de l'affirmation
+			individuelle\xA0?] Et maintenant, dans l'immeuble, un ascenseur d\xE9livre \xE0 son tour son
+			chargement d'employ\xE9s. Parmi ceux-ci, pris dans le mouvement g\xE9n\xE9ral, un homme continue,
+			imperturbable, de dicter \xE0 sa secr\xE9taire quand un portier le voit passer \xE0 sa hauteur et
+			l'interpelle avec enjouement, lui seul parmi tous\xA0:\xA0\xAB\xA0Mr.
+			Thornhill\xA0!\xA0\xBB`),$=C(),c=i("p"),H=n(`Thornhill r\xE9plique au salut du portier par un bref mot d'esprit mais ne s'arr\xEAte pas, ne
+			ralentit m\xEAme pas, port\xE9 par le flux des salari\xE9s qui l'entourent et son propre `),N=i("em"),I=n("flow"),J=n(`,
+			le bourdonnement du monologue dont il abreuve sa secr\xE9taire. (Et tandis qu'ils vont vers la
+			sortie, deux autres ascenseurs apparaissent encore dans le champ pour se d\xE9verser \xE0
+			l'identique.)`),S=C(),p=i("p"),z=n(`Dans le cours du mouvement qui donne son impulsion aux premi\xE8res minutes de
+			`),T=i("em"),K=n("North by Northwest"),O=n(`, il faudrait suspendre cet instant o\xF9 le nom de Thornhill est
+			prononc\xE9. [Celui qui nous est ainsi d\xE9sign\xE9, Cary Grant impeccable dans son costume gris,
+			n'h\xE9siterait sans doute pas \xE0 dire, comme Don Quichotte\xA0: \xAB\xA0Je sais qui je
+			suis\xA0\xBB (un publicitaire de Madison Ave., une double divorc\xE9 et le fils de sa maman...),
+			mais quel inconv\xE9nient y aurait-il \xE0 ce qu'un autre vous le rappelle\xA0?] On l'ignore
+			encore, mais cet instant, en faisant nommer son protagoniste, amorce v\xE9ritablement le r\xE9cit
+			(ne serait-ce pas m\xEAme son v\xE9ritable MacGuffin\xA0?), un r\xE9cit qui justement va s'ent\xEAter \xE0
+			vouloir le d\xE9pouiller de son nom et, partant, de son identit\xE9.`),Q=C(),q=i("p"),V=n(`Peu apr\xE8s, Thornhill, ayant rejoint des relations d'affaires au bar de l'h\xF4tel Plaza, fait
+			innocemment le geste qui referme sur lui un pi\xE8ge auquel il est \xE9tranger\xA0: il l\xE8ve la
+			main pour tenter de faire venir un gar\xE7on \xE0 l'instant m\xEAme o\xF9 celui-ci passe au milieu des
+			clients \xE0 la recherche d'un nomm\xE9 Kaplan. Par son geste, Thornhill r\xE9pond une seconde fois en
+			peu de temps \u2013\xA0cette fois-ci sans m\xEAme le savoir\xA0\u2013 \xE0 l'appel d'un nom, et devient,
+			pour les deux inconnus qui ont mont\xE9 le stratag\xE8me et observent la sc\xE8ne \xE0 l'\xE9cart, celui
+			qu'ils cherchent. Et l'ayant ainsi identifi\xE9, le kidnappent.`),this.h()},l(h){u=l(h,"DIV",{class:!0});var g=o(u);t=l(g,"ARTICLE",{class:!0});var m=o(t);r=l(m,"DIV",{class:!0});var B=o(r);b=a(B,y[0]),E=a(B," caract\xE8res."),B.forEach(s),v=j(m),d=l(m,"P",{class:!0});var _=o(d);w=a(_,"C'est sur un nom prononc\xE9 que s'ouvre \u2013\xA0ou peut s'en faut\xA0\u2013 "),x=l(_,"EM",{});var U=o(x);D=a(U,"North by Northwest"),U.forEach(s),A=a(_,`
+			(`),M=l(_,"EM",{});var W=o(M);G=a(W,"La Mort aux trousses"),W.forEach(s),L=a(_,`, Alfred Hitchcock, 1959). Nous sommes dans le hall d'un
+			immeuble de Manhattan \xE0 l'heure de la sortie des bureaux. Dans le quartier, on vient de voir,
+			en une succession de plans d'ensemble, la foule ininterrompue de la fin d'apr\xE8s-midi traverser
+			une avenue, descendre dans une bouche de m\xE9tro, se laisser porter comme un banc de poissons
+			dans l'escalier monumental de Grand Central. [Ne dirait-on l'incarnation id\xE9ale d'une foule
+			affair\xE9e, d\xE9mocratique, immanquablement am\xE9ricaine, o\xF9 conformit\xE9 et \xAB\xA0distinction\xA0\xBB
+			vont de pair, o\xF9 l'anonymat consenti offre en m\xEAme temps la possibilit\xE9 de l'affirmation
+			individuelle\xA0?] Et maintenant, dans l'immeuble, un ascenseur d\xE9livre \xE0 son tour son
+			chargement d'employ\xE9s. Parmi ceux-ci, pris dans le mouvement g\xE9n\xE9ral, un homme continue,
+			imperturbable, de dicter \xE0 sa secr\xE9taire quand un portier le voit passer \xE0 sa hauteur et
+			l'interpelle avec enjouement, lui seul parmi tous\xA0:\xA0\xAB\xA0Mr.
+			Thornhill\xA0!\xA0\xBB`),_.forEach(s),$=j(m),c=l(m,"P",{class:!0});var k=o(c);H=a(k,`Thornhill r\xE9plique au salut du portier par un bref mot d'esprit mais ne s'arr\xEAte pas, ne
+			ralentit m\xEAme pas, port\xE9 par le flux des salari\xE9s qui l'entourent et son propre `),N=l(k,"EM",{});var X=o(N);I=a(X,"flow"),X.forEach(s),J=a(k,`,
+			le bourdonnement du monologue dont il abreuve sa secr\xE9taire. (Et tandis qu'ils vont vers la
+			sortie, deux autres ascenseurs apparaissent encore dans le champ pour se d\xE9verser \xE0
+			l'identique.)`),k.forEach(s),S=j(m),p=l(m,"P",{class:!0});var P=o(p);z=a(P,`Dans le cours du mouvement qui donne son impulsion aux premi\xE8res minutes de
+			`),T=l(P,"EM",{});var Y=o(T);K=a(Y,"North by Northwest"),Y.forEach(s),O=a(P,`, il faudrait suspendre cet instant o\xF9 le nom de Thornhill est
+			prononc\xE9. [Celui qui nous est ainsi d\xE9sign\xE9, Cary Grant impeccable dans son costume gris,
+			n'h\xE9siterait sans doute pas \xE0 dire, comme Don Quichotte\xA0: \xAB\xA0Je sais qui je
+			suis\xA0\xBB (un publicitaire de Madison Ave., une double divorc\xE9 et le fils de sa maman...),
+			mais quel inconv\xE9nient y aurait-il \xE0 ce qu'un autre vous le rappelle\xA0?] On l'ignore
+			encore, mais cet instant, en faisant nommer son protagoniste, amorce v\xE9ritablement le r\xE9cit
+			(ne serait-ce pas m\xEAme son v\xE9ritable MacGuffin\xA0?), un r\xE9cit qui justement va s'ent\xEAter \xE0
+			vouloir le d\xE9pouiller de son nom et, partant, de son identit\xE9.`),P.forEach(s),Q=j(m),q=l(m,"P",{class:!0});var Z=o(q);V=a(Z,`Peu apr\xE8s, Thornhill, ayant rejoint des relations d'affaires au bar de l'h\xF4tel Plaza, fait
+			innocemment le geste qui referme sur lui un pi\xE8ge auquel il est \xE9tranger\xA0: il l\xE8ve la
+			main pour tenter de faire venir un gar\xE7on \xE0 l'instant m\xEAme o\xF9 celui-ci passe au milieu des
+			clients \xE0 la recherche d'un nomm\xE9 Kaplan. Par son geste, Thornhill r\xE9pond une seconde fois en
+			peu de temps \u2013\xA0cette fois-ci sans m\xEAme le savoir\xA0\u2013 \xE0 l'appel d'un nom, et devient,
+			pour les deux inconnus qui ont mont\xE9 le stratag\xE8me et observent la sc\xE8ne \xE0 l'\xE9cart, celui
+			qu'ils cherchent. Et l'ayant ainsi identifi\xE9, le kidnappent.`),Z.forEach(s),m.forEach(s),g.forEach(s),this.h()},h(){f(r,"class","count svelte-k34rbs"),f(d,"class","svelte-k34rbs"),f(c,"class","svelte-k34rbs"),f(p,"class","svelte-k34rbs"),f(q,"class","svelte-k34rbs"),f(t,"class","svelte-k34rbs"),f(u,"class","container svelte-k34rbs")},m(h,g){se(h,u,g),e(u,t),e(t,r),e(r,b),e(r,E),e(t,v),e(t,d),e(d,w),e(d,x),e(x,D),e(d,A),e(d,M),e(M,G),e(d,L),e(t,$),e(t,c),e(c,H),e(c,N),e(N,I),e(c,J),e(t,S),e(t,p),e(p,z),e(p,T),e(T,K),e(p,O),e(t,Q),e(t,q),e(q,V),R||(F=re(y[1].call(null,t)),R=!0)},p(h,[g]){g&1&&ie(b,h[0])},i:ee,o:ee,d(h){h&&s(u),R=!1,F()}}}function oe(y,u,t){let r=0;function b(E){E.querySelectorAll("p").forEach(v=>{t(0,r=r+v.textContent.replace(/(\\[nrt]|(\[[^\]]+\]))/gi,"").length),v.innerHTML=v.innerHTML.replace(/(\[[^\]]+\])/gi,"<span style='opacity: .5;'>$1</span>")})}return[r,b]}class de extends te{constructor(u){super();ne(this,u,oe,le,ae,{})}}export{de as default};
