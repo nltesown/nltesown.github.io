c3419fb901ced3750759f7e383f367ec337b2026
nltesown <nltesown@users.noreply.github.com>
2022-05-31 17:17:46 +0000

    deploy: 5ee361d50f666828a08c62269b3adde800d08619

---
+++ _app/pages/nxnwbu.svelte-6138aa5c.js
@@ -0,0 +1,29 @@
+import{S as V,i as J,s as B,e as l,t as p,k as x,c as u,a as o,h as m,d as a,m as C,b as h,g as F,I as e,a5 as K,j as O,J as H}from"../chunks/vendor-ff1b9e40.js";function Q(E){let n,t,s,v,y,i,d,q,M,T,k,f,A,P,b,N,$,g,w,z,j;return{c(){n=l("div"),t=l("article"),s=l("div"),v=p(E[0]),y=p(" caract\xE8res."),i=x(),d=l("p"),q=l("em"),M=p("North by Northwest"),T=p(` s'ouvre\xA0\u2013\xA0ou peut s'en faut\xA0\u2013\xA0sur un nom. C'est
+			l'heure de la sortie des bureaux \xE0 Manhattan. En une succession de plans d'ensemble, une foule
+			ininterrompue, rapide, circule dans tous les sens, se d\xE9verse par les portes \xE0 tambour d'un building,
+			s'enfonce dans une bouche de m\xE9tro, se laisse porter par le courant dans l'escalier monumental
+			de Grand Central. Puis, dans le hall d'un immeuble de bureaux o\xF9 les ascenseurs \xE0 cette heure-l\xE0
+			d\xE9livrent en continu leurs chargements d'employ\xE9s, un homme suit le mouvement sans cesser de dicter
+			un m\xE9mo \xE0 sa secr\xE9taire, quand un groom l'aper\xE7oit et l'interpelle \xE0 son passage avec enjouement.`),k=x(),f=l("p"),A=p("\xAB\xA0Mr. Thornhill\xA0!\xA0\xBB"),P=x(),b=l("p"),N=p(`\xC0 l'inverse du nomm\xE9 Thornhill, qui r\xE9pond au salut du groom par un mot d'esprit express mais
+			ne s'arr\xEAte pas, ne ralentit m\xEAme pas, nous devrions, nous, nous arr\xEAter un instant \xE0 cette
+			premi\xE8re parole vraiment audible du film par laquelle le protagoniste est d'embl\xE9e d\xE9sign\xE9 par
+			son nom. Avec ce pressentiment\xA0: le film entier pourra fera le r\xE9cit des m\xE9saventures de
+			ce nom et\xA0\u2013\xA0derri\xE8re ce mince paravent du nom\xA0\u2013\xA0de l'identit\xE9 \xE0 nu de son
+			personnage.`),$=x(),g=l("p"),w=p(`Et \xE0 cet instant sur lequel le film ne s'attarde pas, parfaitement anodin en apparence, r\xE9pond
+			peu de temps apr\xE8s le geste qui met effectivement en marche le r\xE9cit. Roger Thornhill (Cary
+			Grant) est un publicitaire \xE0 succ\xE8s de Madison Avenue. Apr\xE8s sa sortie du bureau, il se rend \xE0
+			un rendez-vous d'affaires au bar de l'h\xF4tel Plaza.`),this.h()},l(c){n=u(c,"DIV",{class:!0});var _=o(n);t=u(_,"ARTICLE",{class:!0});var r=o(t);s=u(r,"DIV",{class:!0});var G=o(s);v=m(G,E[0]),y=m(G," caract\xE8res."),G.forEach(a),i=C(r),d=u(r,"P",{class:!0});var I=o(d);q=u(I,"EM",{});var L=o(q);M=m(L,"North by Northwest"),L.forEach(a),T=m(I,` s'ouvre\xA0\u2013\xA0ou peut s'en faut\xA0\u2013\xA0sur un nom. C'est
+			l'heure de la sortie des bureaux \xE0 Manhattan. En une succession de plans d'ensemble, une foule
+			ininterrompue, rapide, circule dans tous les sens, se d\xE9verse par les portes \xE0 tambour d'un building,
+			s'enfonce dans une bouche de m\xE9tro, se laisse porter par le courant dans l'escalier monumental
+			de Grand Central. Puis, dans le hall d'un immeuble de bureaux o\xF9 les ascenseurs \xE0 cette heure-l\xE0
+			d\xE9livrent en continu leurs chargements d'employ\xE9s, un homme suit le mouvement sans cesser de dicter
+			un m\xE9mo \xE0 sa secr\xE9taire, quand un groom l'aper\xE7oit et l'interpelle \xE0 son passage avec enjouement.`),I.forEach(a),k=C(r),f=u(r,"P",{class:!0});var R=o(f);A=m(R,"\xAB\xA0Mr. Thornhill\xA0!\xA0\xBB"),R.forEach(a),P=C(r),b=u(r,"P",{class:!0});var S=o(b);N=m(S,`\xC0 l'inverse du nomm\xE9 Thornhill, qui r\xE9pond au salut du groom par un mot d'esprit express mais
+			ne s'arr\xEAte pas, ne ralentit m\xEAme pas, nous devrions, nous, nous arr\xEAter un instant \xE0 cette
+			premi\xE8re parole vraiment audible du film par laquelle le protagoniste est d'embl\xE9e d\xE9sign\xE9 par
+			son nom. Avec ce pressentiment\xA0: le film entier pourra fera le r\xE9cit des m\xE9saventures de
+			ce nom et\xA0\u2013\xA0derri\xE8re ce mince paravent du nom\xA0\u2013\xA0de l'identit\xE9 \xE0 nu de son
+			personnage.`),S.forEach(a),$=C(r),g=u(r,"P",{class:!0});var D=o(g);w=m(D,`Et \xE0 cet instant sur lequel le film ne s'attarde pas, parfaitement anodin en apparence, r\xE9pond
+			peu de temps apr\xE8s le geste qui met effectivement en marche le r\xE9cit. Roger Thornhill (Cary
+			Grant) est un publicitaire \xE0 succ\xE8s de Madison Avenue. Apr\xE8s sa sortie du bureau, il se rend \xE0
+			un rendez-vous d'affaires au bar de l'h\xF4tel Plaza.`),D.forEach(a),r.forEach(a),_.forEach(a),this.h()},h(){h(s,"class","count svelte-k34rbs"),h(d,"class","svelte-k34rbs"),h(f,"class","svelte-k34rbs"),h(b,"class","svelte-k34rbs"),h(g,"class","svelte-k34rbs"),h(t,"class","svelte-k34rbs"),h(n,"class","container svelte-k34rbs")},m(c,_){F(c,n,_),e(n,t),e(t,s),e(s,v),e(s,y),e(t,i),e(t,d),e(d,q),e(q,M),e(d,T),e(t,k),e(t,f),e(f,A),e(t,P),e(t,b),e(b,N),e(t,$),e(t,g),e(g,w),z||(j=K(E[1].call(null,t)),z=!0)},p(c,[_]){_&1&&O(v,c[0])},i:H,o:H,d(c){c&&a(n),z=!1,j()}}}function U(E,n,t){let s=0;function v(y){y.querySelectorAll("p").forEach(i=>{t(0,s=s+i.textContent.replace(/(\\[nrt]|(\[[^\]]+\]))/gi,"").length),i.innerHTML=i.innerHTML.replace(/(\[[^\]]+\])/gi,"<span style='opacity: .5;'>$1</span>")})}return[s,v]}class X extends V{constructor(n){super();J(this,n,U,Q,B,{})}}export{X as default};
